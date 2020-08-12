305dd39b1d7fecd11b0db7a08df81af635c0fbb6
Mostafa Talaat <mostafatalaat770@gmail.com>
2020-08-12 14:20:39 +0200

    models: bugfixes
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- backend/models/Article.js
+++ backend/models/Article.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
 
 const schema = new mongoose.Schema({
-    title: {type :String,required: true  },
-    content: {type :String,required: true  },
-    userID: { type: mongoose.Schema.Types.ObjectId , ref: 'User', required :true },
-    username:{type :String,required: true  },
-    likes:Number,
-    likeList:[{username:String,userID:{type: mongoose.Schema.Types.ObjectId,ref: 'User'}}],
-    comments :[{ body : String , username:String,userID:{type: mongoose.Schema.Types.ObjectId,ref: 'User'}}],
-    readers :Number,
-    tags :[String],
-    //time:Date
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  username: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+  likeList: [{ username: String, userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } }],
+  comments: [{ body: String, username: String, userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } }],
+  readers: { type: Number, default: 0 },
+  tags: [{ type: String }],
+  time: { type: Date, default: new Date() }
 
-})
-schema.set('toJSON', {})
-schema.plugin(uniqueValidator);
+});
+
 module.exports = mongoose.model('Article', schema);
--- backend/models/User.js
+++ backend/models/User.js
@@ -11,17 +11,11 @@ const schema = new mongoose.Schema({
   followers: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'User' }],
   history: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Article' }],
   savedArticles: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Article' }],
-  articles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }], // edited
+  articles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
   perferences: [{ type: String }]
 
 });
 
-schema.set('toJSON', (document, returnedObject) => {
-  returnedObject.id = returnedObject.id.toString();
-  delete returnedObject._id;
-  delete returnedObject.__v;
-  delete returnedObject.passwordHash;
-});
 schema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', schema);
